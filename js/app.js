5f9221fecf902dc182d2745461ceb1f350e37a10
martingalli <martin.galli@wunderman.com>
2018-04-19 18:16:18 -0300

    Active roll of dice

--- js/app.js
+++ js/app.js
@@ -9,14 +9,43 @@ GAME RULES:
 
 */
 
-let scores, roundScore, activePlayer, dice
+let scores, roundScore, activePlayer
 
 scores = [0, 0]
 roundScore = 0
-activePlayer = 1
+activePlayer = 0
 
-dice = Math.floor(Math.random() * 6) + 1
-
-document.querySelector(`#current-${activePlayer}`).textContent = dice
+// document.querySelector(`#current-${activePlayer}`).textContent = dice
 // document.querySelector(`#current-${activePlayer}`).innerHTML = `<em>${dice}</em>`
 
+document.querySelector('.dice').style.display = 'none'
+
+document.getElementById('score-0').textContent = '0'
+document.getElementById('score-1').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+document.getElementById('current-0').textContent = '0'
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    let dice = Math.floor(Math.random() * 6) + 1
+
+    let diceDOM = document.querySelector('.dice')
+    diceDOM.style.display = 'block'
+    diceDOM.src = `images/dice-${dice}.png`
+
+    if (dice !== 1) {
+        roundScore += dice
+        document.querySelector(`#current-${activePlayer}`).textContent = roundScore
+    } else {
+         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+         roundScore = 0
+
+         document.getElementById('current-0').textContent = '0'
+         document.getElementById('current-1').textContent = '0'
+
+         document.querySelector('.player-0-panel').classList.toggle('active')
+         document.querySelector('.player-1-panel').classList.toggle('active')
+
+         diceDOM.style.display = 'none'
+    }
+    
+})
