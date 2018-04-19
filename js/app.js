7238d2c7c6c47c0dc2379cbe63366d33cc8d1eff
martingalli <martin.galli@wunderman.com>
2018-04-19 18:30:49 -0300

    Hold button funcionality

--- js/app.js
+++ js/app.js
@@ -25,7 +25,7 @@ document.getElementById('score-1').textContent = '0'
 document.getElementById('current-0').textContent = '0'
 document.getElementById('current-0').textContent = '0'
 
-document.querySelector('.btn-roll').addEventListener('click', function() {
+document.querySelector('.btn-roll').addEventListener('click', () => {
     let dice = Math.floor(Math.random() * 6) + 1
 
     let diceDOM = document.querySelector('.dice')
@@ -36,16 +36,29 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore += dice
         document.querySelector(`#current-${activePlayer}`).textContent = roundScore
     } else {
-         activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
-         roundScore = 0
-
-         document.getElementById('current-0').textContent = '0'
-         document.getElementById('current-1').textContent = '0'
-
-         document.querySelector('.player-0-panel').classList.toggle('active')
-         document.querySelector('.player-1-panel').classList.toggle('active')
-
-         diceDOM.style.display = 'none'
+        nextPlayer()
     }
     
 })
+
+document.querySelector('.btn-hold').addEventListener('click', () => {
+    // Add CURRENT score to GLOBAL score
+    scores[activePlayer] += roundScore
+    // Update the UI
+    document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer]
+    // Check if player won the game
+    nextPlayer()
+})
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    diceDOM.style.display = 'none'
+}
